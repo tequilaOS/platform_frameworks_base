909e4152cfcac52ca30ccf40ae71c887ec0016c4
Keith Mok <kmok@cyngn.com>
2016-02-04 11:10:22 -0800

    SystemUI: Add caffeine QS tile
    Co-authored-by: chillger <alexander.loskutov@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Change-Id: I0b9a498285930cc397b7c4d20ddec3f447e6d332

--- packages/SystemUI/src/com/android/systemui/qs/dagger/tequila/QSModuletequila.kt
+++ packages/SystemUI/src/com/android/systemui/qs/dagger/tequila/QSModuletequila.kt
@@ -17,6 +17,7 @@
 package com.android.systemui.qs.dagger.tequila
 
 import com.android.systemui.qs.tileimpl.QSTileImpl
+import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.CellularTile
 import com.android.systemui.qs.tiles.PowerShareTile
 import com.android.systemui.qs.tiles.HeadsUpTile
@@ -29,6 +30,12 @@ import dagger.multibindings.StringKey
 @Module
 interface QSModuletequila {
 
+    /** Inject CaffeineTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(CaffeineTile.TILE_SPEC)
+    fun bindCaffeineTile(caffeineTile: CaffeineTile): QSTileImpl<*>
+    
     /** Inject CellularTile into tileMap in QSModule */
     @Binds
     @IntoMap
