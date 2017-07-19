a442f95343273fe8019644806ea65f617ef1c931
Pranav Vashi <neobuddy89@gmail.com>
2017-07-19 23:46:45 +0530

    base: Allow using 4G icon instead of LTE [1/2]
    Change-Id: Ieb912c64a60ddd42fe7b21d18dadea293ff159c5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5559,6 +5559,12 @@ public final class Settings {
         public static final String SHOW_BATTERY_PERCENT_CHARGING = "status_bar_show_battery_percent_charging";
 
         /**
+         * Whether to display 4G icon instead LTE
+         * @hide
+         */
+        public static final String SHOW_FOURG_ICON = "show_fourg_icon";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
