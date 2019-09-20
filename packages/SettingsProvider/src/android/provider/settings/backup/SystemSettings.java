ae92c3210139464f3ae0e6d4be8c71f26410ad03
LuK1337 <priv.luk@gmail.com>
2019-09-20 18:30:03 +0200

    base: Add navbar layout inversion tuning [1/2]
    * Originally implemented by jesec but since
      then has been reworked by me to use RTL
      instead of replacing left / right buttons.
    
    idoybh:
    * Use existing settings observer instead of tunables
    * Properly add the setting to Settings.java So we can back it up
    * Get rid of useless global var
    
    @jhonboy121:
    * use NavigationBarModeController to update layout on settings change
    
    Change-Id: I6159f0232ba2ee9f45d2afbba7d11072d902d9ac
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -94,5 +94,6 @@ public class SystemSettings {
         Settings.System.NETWORK_TRAFFIC_FONT_SIZE,
         Settings.System.NETWORK_TRAFFIC_VIEW_LOCATION,
         Settings.System.FULLSCREEN_GESTURES,
+        Settings.System.NAVIGATION_BAR_INVERSE,
     };
 }
