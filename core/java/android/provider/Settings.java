ed78bb56a65efab029eb7ae58f906bf4e1905883
SuperDroidBond <superdroidbond@yahoo.com>
2019-10-09 16:20:51 +0530

    Network Traffic: Add Option for Users to select size of Single Traffic (Up/Down) Font Size
    Thanks to @SKULSHADY's commit - Havoc-OS/android_frameworks_base@20e6c63#diff-a6b14ee550885fae2c9316575fd6ce9c
    
    idoybh: Add settings value to backup array
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I49ea61d56d0cb28523b70d3d3971a6508c927bac

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5549,6 +5549,13 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_ARROW = "network_traffic_arrow";
 
         /**
+         * What size to show for network traffic indicator in statusbar
+         * @hide
+         */
+        @Readable
+        public static final String NETWORK_TRAFFIC_FONT_SIZE = "network_traffic_font_size";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -5675,6 +5682,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_TYPE);
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD);
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_ARROW);
+            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_FONT_SIZE);
         }
 
         /**
