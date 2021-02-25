4e1a81d77af269ac43f7e764546a00bd2ceff7f8
Oliver Scott <olivercscott@gmail.com>
2021-02-25 11:29:35 -0500

    base: Wi-Fi timeout feature (1/2)
    SDB - adapt for A12 FLAG_MUTABLE
    
    Issue: calyxos#228
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Iaecf7f74de04f255abb2563eafa61c2951bc4b08

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -15175,6 +15175,12 @@ public final class Settings {
         public static final String ONE_HANDED_KEYGUARD_SIDE = "one_handed_keyguard_side";
 
         /**
+         * The amount of time in milliseconds before wifi is turned off
+         * @hide
+         */
+        public static final String WIFI_OFF_TIMEOUT = "wifi_off_timeout";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
