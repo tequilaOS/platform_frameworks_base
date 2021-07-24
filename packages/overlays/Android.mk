f043db037e8f576f01d77ca3eef8b86e848e5789
AshutoshSundresh <ashutoshsundresh@gmail.com>
2021-07-24 17:13:07 +0530

    overlays: Add an OxygenOS icon pack
    @neobuddy89:
    * Clean up unused resources (like dual volte slot, brightness high/low)
    * Do not change code for drawables. Make drawables compatible instead.
    
    Co-authored-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Co-authored-by: Anushek Prasal <anushekprasal@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I1a039cee023053e6342148c592c1c705d153ab42

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -61,17 +61,19 @@ LOCAL_REQUIRED_MODULES := \
 	NavigationBarModeGesturalOverlayNarrowBack \
 	NavigationBarModeGesturalOverlayWideBack \
 	NavigationBarModeGesturalOverlayExtraWideBack \
-    IconShapeCylinderOverlay \
-    IconShapeHeartOverlay \
-    IconShapeHexagonOverlay \
-    IconShapeLeafOverlay \
-    IconShapePebbleOverlay \
-    IconShapeRoundedRectOverlay \
-    IconShapeSquareOverlay \
-    IconShapeSquircleOverlay \
-    IconShapeTaperedRectOverlay \
-    IconShapeTeardropOverlay \
-    IconShapeVesselOverlay \
+	IconPackOOSAndroidOverlay \
+	IconPackOOSSystemUIOverlay \
+	IconShapeCylinderOverlay \
+	IconShapeHeartOverlay \
+	IconShapeHexagonOverlay \
+	IconShapeLeafOverlay \
+	IconShapePebbleOverlay \
+	IconShapeRoundedRectOverlay \
+	IconShapeSquareOverlay \
+	IconShapeSquircleOverlay \
+	IconShapeTaperedRectOverlay \
+	IconShapeTeardropOverlay \
+	IconShapeVesselOverlay \
 	preinstalled-packages-platform-overlays.xml
 
 include $(BUILD_PHONY_PACKAGE)
