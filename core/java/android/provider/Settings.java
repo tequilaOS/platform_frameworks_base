76d07dd61c86514ca74d883a5178c280fbc9f433
jhonboy121 <alfredmathew05@gmail.com>
2021-11-25 17:32:41 +0530

    base: allow hiding navbar [1/3]
    Change-Id: I1f8cc8df25cbb56c2f6194fada0afec68d8b7259
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5650,6 +5650,13 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_LOCKSCREEN = "double_tap_sleep_lockscreen";
 
         /**
+         * Whether to hide navbar pill and keyboard space.
+         * Default 0.
+         * @hide
+         */
+        public static final String FULLSCREEN_GESTURES = "fullscreen_gestures";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
