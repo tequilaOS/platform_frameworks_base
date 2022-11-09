9b0ae0078c39bb45c6e72167bac5908e1798191b
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-11-10 01:20:15 +0530

    SystemUI: Make advanced reboot optional
    Allow toggling it, and disable it by default.
    
    Change-Id: I58bdabe2d3d25612bfb01fc842fd5a80168b7537

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5755,6 +5755,12 @@ public final class Settings {
                 "screen_flash_notification_color_global";
 
         /**
+         * Whether to show advanced reboot options in power menu
+         * @hide
+         */
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
