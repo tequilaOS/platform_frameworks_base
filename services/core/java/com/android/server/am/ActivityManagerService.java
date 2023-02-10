e26bb62e07a60026193ef8f137fa23ac85af52f6
Guanyou Chen <chenguanyou9338@gmail.com>
2023-02-10 14:34:03 +0000

    BugFix:AMS.dump dumpAppId invalid
    exp: dumpsys activity processes [package]
    Test: manual
    
    Change-Id: Icf289dd9db04fd4211d7d149e6e9ded321bde0d7

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -9650,6 +9650,7 @@ public class ActivityManagerService extends IActivityManager.Stub
             } else if ("processes".equals(cmd) || "p".equals(cmd)) {
                 if (opti < args.length) {
                     dumpPackage = args[opti];
+                    dumpAppId = getAppId(dumpPackage);
                     opti++;
                 }
                 synchronized (this) {
