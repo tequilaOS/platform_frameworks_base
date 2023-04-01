PixelExperience/frameworks_base

Initial import of lineagehw
All credits to LineageOS and related authors on:

https://github.com/LineageOS/android_lineage-sdk

Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
Change-Id: Ief90ce8c20115d6a1c17c84a9874af6246dc1dcd

core/java/com/android/internal/custom/app/LineageContextConstants.java
@@ -1,0 +1,46 @@
+/**
+ * Copyright (c) 2015, The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.custom.app;
+
+import android.annotation.SdkConstant;
+
+/**
+ * @hide
+ * TODO: We need to somehow make these managers accessible via getSystemService
+ */
+public final class LineageContextConstants {
+
+    /**
+     * @hide
+     */
+    private LineageContextConstants() {
+        // Empty constructor
+    }
+
+    /**
+     * Use with {@link android.content.Context#getSystemService} to retrieve a
+     * {@link com.android.internal.custom.LineageHardwareManager} to manage the extended
+     * hardware features of the device.
+     *
+     * @see android.content.Context#getSystemService
+     * @see com.android.internal.custom.LineageHardwareManager
+     *
+     * @hide
+     */
+    public static final String LINEAGE_HARDWARE_SERVICE = "lineagehardware";
+
+}

core/java/com/android/internal/custom/hardware/HIDLHelper.java
@@ -1,0 +1,22 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.custom.hardware;
+
+import java.util.ArrayList;
+
+class HIDLHelper {
+}

core/java/com/android/internal/custom/hardware/ILineageHardwareService.aidl
@@ -1,0 +1,25 @@
+/**
+ * Copyright (c) 2015-2016 The CyanogenMod Project
+ *               2017-2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.custom.hardware;
+
+/** @hide */
+interface ILineageHardwareService {
+    int getSupportedFeatures();
+    boolean get(int feature);
+    boolean set(int feature, boolean enable);
+}

services/core/java/com/android/server/custom/LineageHardwareService.java
@@ -1,0 +1,145 @@
+/*
+ * Copyright (C) 2015-2016 The CyanogenMod Project
+ *               2017-2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.server.custom;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.RemoteCallbackList;
+import android.os.RemoteException;
+import android.os.UserHandle;
+import android.util.Log;
+import android.util.Range;
+
+import com.android.server.LocalServices;
+import com.android.server.SystemService;
+
+import com.android.internal.custom.hardware.ILineageHardwareService;
+import com.android.internal.custom.hardware.LineageHardwareManager;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.android.internal.custom.app.LineageContextConstants;
+
+
+/** @hide */
+public class LineageHardwareService extends SystemService {
+
+    private static final boolean DEBUG = true;
+    private static final String TAG = LineageHardwareService.class.getSimpleName();
+
+    private final Context mContext;
+    private final LineageHardwareInterface mLineageHwImpl;
+
+    private interface LineageHardwareInterface {
+        public int getSupportedFeatures();
+        public boolean get(int feature);
+        public boolean set(int feature, boolean enable);
+    }
+
+    private class LegacyLineageHardware implements LineageHardwareInterface {
+
+        private int mSupportedFeatures = 0;
+
+        public LegacyLineageHardware() {
+        }
+
+        public int getSupportedFeatures() {
+            return mSupportedFeatures;
+        }
+
+        public boolean get(int feature) {
+            /*switch(feature) {
+                default:
+                    Log.e(TAG, "feature " + feature + " is not a boolean feature");
+                    return false;
+            }*/
+        }
+
+        public boolean set(int feature, boolean enable) {
+            /*switch(feature) {
+                default:
+                    Log.e(TAG, "feature " + feature + " is not a boolean feature");
+                    return false;
+            }*/
+        }
+    }
+
+    private LineageHardwareInterface getImpl(Context context) {
+        return new LegacyLineageHardware();
+    }
+
+    public LineageHardwareService(Context context) {
+        super(context);
+        mContext = context;
+        mLineageHwImpl = getImpl(context);
+        publishBinderService(LineageContextConstants.LINEAGE_HARDWARE_SERVICE, mService);
+    }
+
+    @Override
+    public void onBootPhase(int phase) {
+        if (phase == PHASE_BOOT_COMPLETED) {
+            Intent intent = new Intent("lineageos.intent.action.INITIALIZE_LINEAGE_HARDWARE");
+            intent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
+            mContext.sendBroadcastAsUser(intent, UserHandle.ALL,
+                    "lineageos.permission.HARDWARE_ABSTRACTION_ACCESS");
+        }
+    }
+
+    @Override
+    public void onStart() {
+    }
+
+    private final IBinder mService = new ILineageHardwareService.Stub() {
+
+        private boolean isSupported(int feature) {
+            return (getSupportedFeatures() & feature) == feature;
+        }
+
+        @Override
+        public int getSupportedFeatures() {
+            mContext.enforceCallingOrSelfPermission(
+                    "lineageos.permission.HARDWARE_ABSTRACTION_ACCESS", null);
+            return mLineageHwImpl.getSupportedFeatures();
+        }
+
+        @Override
+        public boolean get(int feature) {
+            mContext.enforceCallingOrSelfPermission(
+                    "lineageos.permission.HARDWARE_ABSTRACTION_ACCESS", null);
+            if (!isSupported(feature)) {
+                Log.e(TAG, "feature " + feature + " is not supported");
+                return false;
+            }
+            return mLineageHwImpl.get(feature);
+        }
+
+        @Override
+        public boolean set(int feature, boolean enable) {
+            mContext.enforceCallingOrSelfPermission(
+                    "lineageos.permission.HARDWARE_ABSTRACTION_ACCESS", null);
+            if (!isSupported(feature)) {
+                Log.e(TAG, "feature " + feature + " is not supported");
+                return false;
+            }
+            return mLineageHwImpl.set(feature, enable);
+        }
+    };
+}

tequilaOS/platform_frameworks_base

core/java/com/android/internal/tequila/app/LineageContextConstants.java
@@ -1,0 +1,46 @@
+/**
+ * Copyright (c) 2015, The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.tequila.app;
+
+import android.annotation.SdkConstant;
+
+/**
+ * @hide
+ * TODO: We need to somehow make these managers accessible via getSystemService
+ */
+public final class LineageContextConstants {
+
+    /**
+     * @hide
+     */
+    private LineageContextConstants() {
+        // Empty constructor
+    }
+
+    /**
+     * Use with {@link android.content.Context#getSystemService} to retrieve a
+     * {@link com.android.internal.tequila.LineageHardwareManager} to manage the extended
+     * hardware features of the device.
+     *
+     * @see android.content.Context#getSystemService
+     * @see com.android.internal.tequila.LineageHardwareManager
+     *
+     * @hide
+     */
+    public static final String LINEAGE_HARDWARE_SERVICE = "lineagehardware";
+
+}

core/java/com/android/internal/tequila/hardware/HIDLHelper.java
@@ -1,0 +1,22 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.tequila.hardware;
+
+import java.util.ArrayList;
+
+class HIDLHelper {
+}

core/java/com/android/internal/tequila/hardware/ILineageHardwareService.aidl
@@ -1,0 +1,25 @@
+/**
+ * Copyright (c) 2015-2016 The CyanogenMod Project
+ *               2017-2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.tequila.hardware;
+
+/** @hide */
+interface ILineageHardwareService {
+    int getSupportedFeatures();
+    boolean get(int feature);
+    boolean set(int feature, boolean enable);
+}

services/core/java/com/android/server/tequila/LineageHardwareService.java
@@ -1,0 +1,145 @@
+/*
+ * Copyright (C) 2015-2016 The CyanogenMod Project
+ *               2017-2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.server.tequila;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.RemoteCallbackList;
+import android.os.RemoteException;
+import android.os.UserHandle;
+import android.util.Log;
+import android.util.Range;
+
+import com.android.server.LocalServices;
+import com.android.server.SystemService;
+
+import com.android.internal.tequila.hardware.ILineageHardwareService;
+import com.android.internal.tequila.hardware.LineageHardwareManager;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.android.internal.tequila.app.LineageContextConstants;
+
+
+/** @hide */
+public class LineageHardwareService extends SystemService {
+
+    private static final boolean DEBUG = true;
+    private static final String TAG = LineageHardwareService.class.getSimpleName();
+
+    private final Context mContext;
+    private final LineageHardwareInterface mLineageHwImpl;
+
+    private interface LineageHardwareInterface {
+        public int getSupportedFeatures();
+        public boolean get(int feature);
+        public boolean set(int feature, boolean enable);
+    }
+
+    private class LegacyLineageHardware implements LineageHardwareInterface {
+
+        private int mSupportedFeatures = 0;
+
+        public LegacyLineageHardware() {
+        }
+
+        public int getSupportedFeatures() {
+            return mSupportedFeatures;
+        }
+
+        public boolean get(int feature) {
+            switch(feature) {
+                default:
+                    Log.e(TAG, "feature " + feature + " is not a boolean feature");
+                    return false;
+            }
+        }
+
+        public boolean set(int feature, boolean enable) {
+            switch(feature) {
+                default:
+                    Log.e(TAG, "feature " + feature + " is not a boolean feature");
+                    return false;
+            }
+        }
+    }
+
+    private LineageHardwareInterface getImpl(Context context) {
+        return new LegacyLineageHardware();
+    }
+
+    public LineageHardwareService(Context context) {
+        super(context);
+        mContext = context;
+        mLineageHwImpl = getImpl(context);
+        publishBinderService(LineageContextConstants.LINEAGE_HARDWARE_SERVICE, mService);
+    }
+
+    @Override
+    public void onBootPhase(int phase) {
+        if (phase == PHASE_BOOT_COMPLETED) {
+            Intent intent = new Intent("lineageos.intent.action.INITIALIZE_LINEAGE_HARDWARE");
+            intent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
+            mContext.sendBroadcastAsUser(intent, UserHandle.ALL,
+                    "lineageos.permission.HARDWARE_ABSTRACTION_ACCESS");
+        }
+    }
+
+    @Override
+    public void onStart() {
+    }
+
+    private final IBinder mService = new ILineageHardwareService.Stub() {
+
+        private boolean isSupported(int feature) {
+            return (getSupportedFeatures() & feature) == feature;
+        }
+
+        @Override
+        public int getSupportedFeatures() {
+            mContext.enforceCallingOrSelfPermission(
+                    "lineageos.permission.HARDWARE_ABSTRACTION_ACCESS", null);
+            return mLineageHwImpl.getSupportedFeatures();
+        }
+
+        @Override
+        public boolean get(int feature) {
+            mContext.enforceCallingOrSelfPermission(
+                    "lineageos.permission.HARDWARE_ABSTRACTION_ACCESS", null);
+            if (!isSupported(feature)) {
+                Log.e(TAG, "feature " + feature + " is not supported");
+                return false;
+            }
+            return mLineageHwImpl.get(feature);
+        }
+
+        @Override
+        public boolean set(int feature, boolean enable) {
+            mContext.enforceCallingOrSelfPermission(
+                    "lineageos.permission.HARDWARE_ABSTRACTION_ACCESS", null);
+            if (!isSupported(feature)) {
+                Log.e(TAG, "feature " + feature + " is not supported");
+                return false;
+            }
+            return mLineageHwImpl.set(feature, enable);
+        }
+    };
+}