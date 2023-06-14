f9265299573c40c0149cf5cd624fe275fa6760fd
Michael Bestas <mkbestas@gmail.com>
2023-06-14 11:52:56 +0300

    SystemUI: Import and use tequilaModule for QS tiles
    Change-Id: I16eab8d0ec496bbac7e32eea370c846573349cc9

--- packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
@@ -52,6 +52,7 @@ import com.android.systemui.flags.FlagsModule;
 import com.android.systemui.fragments.FragmentService;
 import com.android.systemui.keyboard.KeyboardModule;
 import com.android.systemui.keyguard.data.BouncerViewModule;
+import com.android.systemui.tequila.tequilaModule;
 import com.android.systemui.log.dagger.LogModule;
 import com.android.systemui.mediaprojection.appselector.MediaProjectionModule;
 import com.android.systemui.model.SysUiState;
@@ -168,6 +169,7 @@ import dagger.Provides;
             FooterActionsModule.class,
             GarbageMonitorModule.class,
             KeyboardModule.class,
+            tequilaModule.class,
             LogModule.class,
             MediaProjectionModule.class,
             MotionToolModule.class,
