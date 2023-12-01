71fffab3d7fa847090b566985fe8c27bc1878770
minaripenguin <minaripenguin@users.noreply.github.com>
2023-12-01 15:09:21 +0800

    SystemUI: Introduce wifi standard icon feature [1/2]
    * icons taken from: a799a35c5527482f2d577783170b0478b8021b3f "base: Refactor Wi-Fi standard icons"
    
    [neobuddy89: Added wifi 7 icon]
    [erfanabdi: Move standard icon to the left side]
    
    Change-Id: I187fc97b9f4ad6d48b6c6f0b1215ffbcb1c972e2
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5960,6 +5960,11 @@ public final class Settings {
         public static final String AUTO_BRIGHTNESS_ONE_SHOT = "auto_brightness_one_shot";
 
         /**
+         * @hide
+         */
+        public static final String WIFI_STANDARD_ICON = "wifi_standard_icon";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
